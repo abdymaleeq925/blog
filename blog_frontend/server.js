26b5ec95f8f38665d85c1808118cfe220f7ddfd6
Abdymalik Batyrkulov <abdymalikbatyrkulov@gmail.com>
2025-02-27 10:28:55 +0300

    fix via express

---
+++ blog_frontend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+
+const path = require('path');
+const app = express();
+
+// Обслуживание статических файлов из папки build
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Перенаправление всех запросов на index.html
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
